fbe68fd8703cbd4cb7362a39e421d446920f02cb
cgyee <cyee2017@fau.edu>
2020-04-26 21:29:27 -0400

    RecylerView is now working, Search function is impelemneted only movie title w/o posters are displayed currently

--- app/src/main/java/com/example/movieapp/MovieAdapter.java
+++ app/src/main/java/com/example/movieapp/MovieAdapter.java
@@ -1,34 +1,43 @@
 package com.example.movieapp;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.InputStream;
+import java.net.URL;
+
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieView> {
-    private int mData;
+    private JSONArray mData;
 
     public class MovieView extends RecyclerView.ViewHolder {
-        TextView text;
+        TextView textView;
+        ImageButton imageButton;
 
-        public MovieView(RelativeLayout layout) {
+        public MovieView(LinearLayout layout) {
             super(layout);
         }
     }
 
-    public  MovieAdapter(int data) {
-        mData = data;
-    }
+    public  MovieAdapter(JSONArray data) { mData = data;}
+    public MovieAdapter() {mData=null;}
 
     @Override
     public MovieAdapter.MovieView onCreateViewHolder(ViewGroup parent, int viewType) {
-        ConstraintLayout constraint = (ConstraintLayout) LayoutInflater.from((parent.getContext())).inflate(R.layout.activity_details, parent, false);
-        RelativeLayout layout = constraint.findViewById(R.id.recycler);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(80, 80);
+        ConstraintLayout constraint = (ConstraintLayout) LayoutInflater.from((parent.getContext())).inflate(R.layout.activity_search_page, parent, false);
+        RecyclerView recyclerView = constraint.findViewById(R.id.recycler);
+        LinearLayout layout = new LinearLayout(recyclerView.getContext());
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100, 100);
         ImageButton image = new ImageButton(layout.getContext());
         image.setImageResource(R.drawable.ic_launcher_background);
         image.setLayoutParams(params);
@@ -36,23 +45,45 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieView> {
         params.topMargin = 40;
         layout.addView(image, params);
 
+        params = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         TextView text = new TextView(layout.getContext());
         text.setText("Testing");
-        params.leftMargin = 80;
+        params.leftMargin =60;
         params.topMargin = 20;
 
         layout.addView(text, params);
 
         MovieView mv = new MovieView(layout);
+        mv.textView = text;
+        mv.imageButton = image;
         return  mv;
     }
 
     @Override
     public void onBindViewHolder(MovieView holder, int position) {
+
+       try {
+           holder.textView.setText(mData.getJSONObject(position).get("Title").toString());
+           holder.imageButton.setImageDrawable(LoadImageFromWebOperations(mData.getJSONObject(position).get("Poster").toString()));
+       }
+       catch (JSONException e){
+
+       }
     }
 
     @Override
     public int getItemCount() {
-        return mData;
+        return mData != null ? mData.length() :0;
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
     }
 }
